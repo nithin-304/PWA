70151ede3badf13845682262b271c99bf8e45e50
nithin-304 <rssnithin304@gmail.com>
2023-02-09 16:53:01 +0530

    sad

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,29 @@
 import React from "react";
 import QRCode from "react-qr-code";
-import Interval from 'react-interval-rerender';
+import Interval from "react-interval-rerender";
 
-function fetchQR(t){
-return(
-<div style={{ height: "auto", margin: "0 auto", maxWidth: 64, width: "100%" }}>
-    <QRCode
-    size={256}
-    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-    value={t}
-    viewBox={`0 0 256 256`}
-    />
-    <div>{t}</div>
-</div>
-);
-};
+function fetchQR(t) {
+  return (
+    <div
+      style={{ height: "auto", margin: "0 auto", maxWidth: 64, width: "100%" }}
+    >
+      <QRCode
+        size={256}
+        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+        value={t}
+        viewBox={`0 0 256 256`}
+      />
+      <div>{t}</div>
+    </div>
+  );
+}
 
-export default function home(){
-return (
-<div><Interval delay={1000}>{() => fetchQR(new Date().toLocaleTimeString()})</Interval></div>
-);
-};
+export default function home() {
+  return (
+    <div>
+      <Interval delay={1000}>
+        {() => fetchQR(new Date().toLocaleTimeString())}
+      </Interval>
+    </div>
+  );
+}
