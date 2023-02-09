eb717d2836ccecf4a544fc37117c682b718ff4aa
nithin-304 <rssnithin304@gmail.com>
2023-02-09 15:31:16 +0530

    jss

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ return(
     <QRCode
     size={256}
     style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-    value={value}
+    value={Nithin}
     viewBox={`0 0 256 256`}
     />
 </div>
