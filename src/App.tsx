735ab1ada39afd114e92df1ebfbba77377e40f05
nithin-304 <rssnithin304@gmail.com>
2023-02-09 13:28:39 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
-import Interval from 'react-interval-rerender'
+
 
 const About = lazy(() => import('./About'));
 const Home =  lazy(() => import('./Home') );
 
-export const Clock = () => (
-  <Interval delay={1000}>{() => new Date().toLocaleTimeString()}</Interval>
-)
+
 
 const App: React.FC = () => (
   <Router>
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Interval from 'react-interval-rerender'
 
 const Home: React.FC = () => (
   <h1>Welcome to your PWA!</h1>
 );
+
+export const Clock = () => (
+  <Interval delay={1000}>{() => new Date().toLocaleTimeString()}</Interval>
+)
 
 export default Home;
